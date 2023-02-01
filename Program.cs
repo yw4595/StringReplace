5abe061b1bb990c8b329a6c3b693898221b7c353
Yanzhi Wang <yw4595@g.rit.edu>
2023-02-01 20:49:11 +0300

    replace no with yes

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringReplace
+{
+    /// <summary>
+    /// Author: Yanzhi Wang
+    /// Purpose: The purpose of this class is to demonstrate how to replace "no" with "yes" in a string in a case-insensitive manner.
+    /// Restrictions: None
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            /// <summary>
+            /// Purpose: The purpose of this method is to replace all occurrences of the word "no" with "yes" in a case-insensitive manner.
+            /// Restrictions: None
+            /// </summary>
+            string pattern = @"\bno\b";
+            string replacement = "yes";
+            string result = System.Text.RegularExpressions.Regex.Replace(input, pattern, replacement, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
